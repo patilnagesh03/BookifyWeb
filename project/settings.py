012b3781bfebfccaae5866729618eb934a7885ee
patilnagesh03 <nageshpatil7311@gmail.com>
2025-07-11 15:36:35 +0530

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -139,8 +139,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # important for collectstatic
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',  # Directory for custom static files
+]
 
 
 
